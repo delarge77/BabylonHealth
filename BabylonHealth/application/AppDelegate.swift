ec55eed0c17d9bca0b0493de52803eb75b5557bd
delarge77 <lastripper@gmail.com>
2017-05-05 18:55:21 +0100

    organizing project and including pods

---
+++ BabylonHealth/application/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  BabylonHealth
+//
+//  Created by Alessandro dos Santos Pinto on 05/05/2017.
+//  Copyright © 2017 Alessandro dos Santos Pinto. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
